jai/OpenClaw

fix(googlechat): preserve thread replies

src/auto-reply/reply/agent-runner-execution.ts
@@ -940,6 +940,8 @@ export async function runAgentTurnWithFallback(params: {
     params.opts?.onAgentRunStart?.(runId);
   };
   const currentMessageId = params.sessionCtx.MessageSidFull ?? params.sessionCtx.MessageSid;
+  const implicitReplyToId =
+    normalizeOptionalString(params.sessionCtx.MessageThreadId) ?? currentMessageId;
   const shouldNotifyUserAboutCompaction =
     runtimeConfig?.agents?.defaults?.compaction?.notifyUser === true;
   const sendCompactionNotice = async (phase: "start" | "end" | "incomplete") => {
@@ -954,7 +956,7 @@ export async function runAgentTurnWithFallback(params: {
           : "🧹 Compaction incomplete";
     const noticePayload = params.applyReplyToMode({
       text,
-      replyToId: currentMessageId,
+      replyToId: implicitReplyToId,
       replyToCurrent: true,
       isCompactionNotice: true,
     });
@@ -1174,7 +1176,8 @@ export async function runAgentTurnWithFallback(params: {
       const blockReplyHandler = params.opts?.onBlockReply
         ? createBlockReplyDeliveryHandler({
             onBlockReply: params.opts.onBlockReply,
-            currentMessageId: params.sessionCtx.MessageSidFull ?? params.sessionCtx.MessageSid,
+            currentMessageId,
+            implicitReplyToId,
             replyThreading: params.replyThreading,
             normalizeStreamingText,
             applyReplyToMode: params.applyReplyToMode,

clawdbot/clawdbot

src/auto-reply/reply/agent-runner-execution.ts
@@ -1797,6 +1797,8 @@ async function runAgentTurnWithFallbackInternal(
     });
   };
   const currentMessageId = params.sessionCtx.MessageSidFull ?? params.sessionCtx.MessageSid;
+  const implicitReplyToId =
+    normalizeOptionalString(params.sessionCtx.MessageThreadId) ?? currentMessageId;
   const notifyUserAboutCompaction = shouldNotifyUserAboutCompaction(runtimeConfig);
   const deliverCompactionNoticePayload = async (noticePayload: ReplyPayload, label: string) => {
     try {
@@ -1815,7 +1817,7 @@ async function runAgentTurnWithFallbackInternal(
     await deliverCompactionNoticePayload(
       createCompactionNoticePayload({
         phase,
-        currentMessageId,
+        currentMessageId: implicitReplyToId,
         applyReplyToMode: params.applyReplyToMode,
       }),
       phase,
@@ -2128,7 +2130,8 @@ async function runAgentTurnWithFallbackInternal(
       const blockReplyHandler = params.opts?.onBlockReply
         ? createBlockReplyDeliveryHandler({
             onBlockReply: params.opts.onBlockReply,
-            currentMessageId: params.sessionCtx.MessageSidFull ?? params.sessionCtx.MessageSid,
+            currentMessageId,
+            implicitReplyToId,
             replyThreading: params.replyThreading,
             normalizeStreamingText,
             applyReplyToMode: params.applyReplyToMode,