9e7c45ff63d4888a2a72350d97a6256166ba53e9
wangmiao0668000666 <wang.miao86@xydigit.com>
2026-07-09 13:12:24 +0800

    fix(google-meet): bound Calendar event lookups (#102157)
    Co-authored-by: Peter Steinberger <steipete@gmail.com>
    (cherry picked from commit 28bbbe4f603c17ca770399f044bd8f25c2c8685f)
    (cherry picked from commit fe03932992ab65ca9d7d0f598aa31c0c8679e49c)

--- extensions/google-meet/src/calendar.ts
+++ extensions/google-meet/src/calendar.ts
@@ -6,6 +6,7 @@ const GOOGLE_CALENDAR_API_BASE_URL = "https://www.googleapis.com/calendar/v3";
 const GOOGLE_CALENDAR_API_HOST = "www.googleapis.com";
 const GOOGLE_MEET_URL_HOST = "meet.google.com";
 const GOOGLE_CALENDAR_EVENTS_SCOPE = "https://www.googleapis.com/auth/calendar.events.readonly";
+const GOOGLE_CALENDAR_REQUEST_TIMEOUT_MS = 30_000;
 
 type GoogleCalendarEventDate = {
   date?: string;
@@ -188,6 +189,7 @@ async function fetchGoogleCalendarEvents(params: {
     },
     policy: { allowedHostnames: [GOOGLE_CALENDAR_API_HOST] },
     auditContext: "google-meet.calendar.events.list",
+    timeoutMs: GOOGLE_CALENDAR_REQUEST_TIMEOUT_MS,
   });
   try {
     if (!response.ok) {
