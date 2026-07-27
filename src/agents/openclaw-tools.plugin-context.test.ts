openclaw/openclaw

Propagate delegated auth through agent runs

src/agents/openclaw-tools.plugin-context.test.ts
@@ -190,6 +190,40 @@ describe("openclaw plugin tool context", () => {
     );
   });
 
+  it("forwards trusted chat type", () => {
+    const result = resolveOpenClawPluginToolInputs({
+      options: {
+        config: {} as never,
+        agentChatType: "direct",
+      },
+    });
+
+    expect(result.context.messageChatType).toBe("direct");
+  });
+
+  it("forwards runtime-only plugin auth context", async () => {
+    const pluginAuth = {
+      getDelegatedAccessToken: vi.fn(async () => ({
+        ok: false as const,
+        reason: "not_configured" as const,
+      })),
+    };
+    const result = resolveOpenClawPluginToolInputs({
+      options: {
+        config: {} as never,
+        pluginAuth,
+      },
+    });
+
+    expect(result.context.auth).toBe(pluginAuth);
+    await expect(
+      result.context.auth?.getDelegatedAccessToken({ provider: "msteams" }),
+    ).resolves.toEqual({
+      ok: false,
+      reason: "not_configured",
+    });
+  });
+
   it("does not inject ambient thread defaults into plugin tools", async () => {
     const executeMock = vi.fn(async () => ({
       content: [{ type: "text" as const, text: "ok" }],

clawdbot/clawdbot

src/agents/openclaw-tools.plugin-context.test.ts
@@ -108,6 +108,40 @@ describe("openclaw plugin tool context", () => {
     });
   });
 
+  it("forwards runtime-only plugin auth context", async () => {
+    const pluginAuth = {
+      getDelegatedAccessToken: vi.fn(async () => ({
+        ok: false as const,
+        reason: "not_configured" as const,
+      })),
+    };
+    const result = resolveOpenClawPluginToolInputs({
+      options: {
+        config: {} as never,
+        pluginAuth,
+      },
+    });
+
+    expect(result.context.auth).toBe(pluginAuth);
+    await expect(
+      result.context.auth?.getDelegatedAccessToken({ provider: "msteams" }),
+    ).resolves.toEqual({
+      ok: false,
+      reason: "not_configured",
+    });
+  });
+
+  it("forwards trusted chat type", () => {
+    const result = resolveOpenClawPluginToolInputs({
+      options: {
+        config: {} as never,
+        currentChatType: "direct",
+      },
+    });
+
+    expect(result.context.chatType).toBe("direct");
+  });
+
   it("does not duplicate provider-qualified active model refs", () => {
     const result = resolveOpenClawPluginToolInputs({
       options: {