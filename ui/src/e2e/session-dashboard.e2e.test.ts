Dingding-leo/openclaw

test(ui): prove the dock resize persists off its clamp
Asserting the post-End width equals SIDEBAR_MIN_WIDTH_PX compared the clamp
to itself across reload, so the check would pass even if the persisted width
were dropped and the column fell back to its minimum. Step off the clamp
first and assert that value survives.

(cherry picked from commit 3636124c326a93b80006ccc4064415ec1a6c2c9f)

ui/src/e2e/session-dashboard.e2e.test.ts
@@ -336,17 +336,22 @@ describeControlUiE2e("Control UI session dashboard stitch", () => {
     const dock = page.locator(
       '.sidebar-region__right-runtime .sidebar-column[data-column-id="chat-column"]',
     );
+    const dockWidth = () => dock.evaluate((element) => getComputedStyle(element).width);
     await divider.focus();
     await page.keyboard.press("End");
-    await expect
-      .poll(() => dock.evaluate((element) => getComputedStyle(element).width))
-      .toBe("260px");
-    const persistedWidth = await dock.evaluate((element) => getComputedStyle(element).width);
+    const clampedWidth = await dockWidth();
+    // End pins the column against its clamp, so step back off it: comparing a
+    // clamped width to itself after reload would pass even if the persisted
+    // width were dropped and the column fell back to its minimum.
+    await page.keyboard.press("ArrowLeft");
+    await page.keyboard.press("ArrowLeft");
+    await expect.poll(dockWidth).not.toBe(clampedWidth);
+    const persistedWidth = await dockWidth();
     expect(persistedWidth).toMatch(/^\d+(?:\.\d+)?px$/u);
 
     await page.reload();
     await dock.waitFor();
-    expect(await dock.evaluate((element) => getComputedStyle(element).width)).toBe(persistedWidth);
+    expect(await dockWidth()).toBe(persistedWidth);
     await expect
       .poll(() =>
         page.locator('.chat-tool-card__preview[data-kind="canvas"] [data-pin-widget]').isDisabled(),

clawdbot/clawdbot

ui/src/e2e/session-dashboard.e2e.test.ts
@@ -354,15 +354,23 @@ describeControlUiE2e("Control UI session dashboard stitch", () => {
 
     const divider = page.locator(".board-session-surface__divider");
     const dock = page.locator(".board-session-surface__chat");
+    const dockHeight = () => dock.evaluate((element) => getComputedStyle(element).height);
     await divider.focus();
     await page.keyboard.press("End");
-    await expect.poll(() => dock.getAttribute("style")).not.toBe("height: 320px");
-    const persistedStyle = await dock.getAttribute("style");
-    expect(persistedStyle).toMatch(/^height: \d+(?:\.\d+)?px$/u);
+    await expect.poll(dockHeight).not.toBe("320px");
+    const clampedHeight = await dockHeight();
+    // End pins the bottom dock against its clamp, so step back off it: comparing
+    // a clamped height to itself after reload would pass if persistence broke and
+    // the dock merely fell back to its minimum.
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("ArrowUp");
+    await expect.poll(dockHeight).not.toBe(clampedHeight);
+    const persistedHeight = await dockHeight();
+    expect(persistedHeight).toMatch(/^\d+(?:\.\d+)?px$/u);
 
     await page.reload();
     await dock.waitFor();
-    expect(await dock.getAttribute("style")).toBe(persistedStyle);
+    expect(await dockHeight()).toBe(persistedHeight);
     await expect
       .poll(() =>
         page.locator('.chat-tool-card__preview[data-kind="canvas"] [data-pin-widget]').isDisabled(),