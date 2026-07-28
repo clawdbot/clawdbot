openclaw/openclaw

fix(gateway): project provider timeout outcomes

src/gateway/server-methods/agent-run-dispatch.ts
@@ -183,27 +183,6 @@ export function dispatchAgentRunFromGateway(params: {
           ? (result?.meta?.stopReason ?? "rpc")
           : undefined;
       const timeoutAttribution = readAgentRunTimeoutAttribution(result?.meta);
-      if (taskTracked) {
-        tryFinalizeTrackedAgentTask({
-          runId: params.runId,
-          status: aborted ? resolveAbortedAgentTaskStatus(stopReason) : "succeeded",
-          terminalSummary: aborted ? "aborted" : "completed",
-          log: params.context.logGateway,
-        });
-      }
-      const payload = {
-        runId: params.runId,
-        status: aborted ? ("timeout" as const) : ("ok" as const),
-        summary: aborted ? "aborted" : "completed",
-        ...(aborted ? { stopReason } : {}),
-        ...(aborted && timeoutAttribution.timeoutPhase
-          ? { timeoutPhase: timeoutAttribution.timeoutPhase }
-          : {}),
-        ...(aborted && timeoutAttribution.providerStarted !== undefined
-          ? { providerStarted: timeoutAttribution.providerStarted }
-          : {}),
-        result,
-      };
       const terminalOutcome = buildAgentRunTerminalOutcome({
         status:
           aborted || result?.meta?.stopReason === "timeout" || timeoutAttribution.timeoutPhase
@@ -217,6 +196,34 @@ export function dispatchAgentRunFromGateway(params: {
         timeoutPhase: timeoutAttribution.timeoutPhase,
         providerStarted: timeoutAttribution.providerStarted,
       });
+      const responseTimedOut = aborted || terminalOutcome.status === "timeout";
+      if (taskTracked) {
+        tryFinalizeTrackedAgentTask({
+          runId: params.runId,
+          status: aborted
+            ? resolveAbortedAgentTaskStatus(stopReason)
+            : responseTimedOut
+              ? "timed_out"
+              : "succeeded",
+          terminalSummary: responseTimedOut ? "aborted" : "completed",
+          log: params.context.logGateway,
+        });
+      }
+      const payload = {
+        runId: params.runId,
+        status: responseTimedOut ? ("timeout" as const) : ("ok" as const),
+        summary: responseTimedOut ? "aborted" : "completed",
+        ...(responseTimedOut && terminalOutcome.stopReason
+          ? { stopReason: terminalOutcome.stopReason }
+          : {}),
+        ...(responseTimedOut && timeoutAttribution.timeoutPhase
+          ? { timeoutPhase: timeoutAttribution.timeoutPhase }
+          : {}),
+        ...(responseTimedOut && timeoutAttribution.providerStarted !== undefined
+          ? { providerStarted: timeoutAttribution.providerStarted }
+          : {}),
+        result,
+      };
       const persistTerminalDedupe = () => {
         setGatewayDedupeEntries({
           dedupe: params.context.dedupe,

clawdbot/clawdbot

src/gateway/server-methods/agent-run-dispatch.ts
@@ -176,23 +176,30 @@ export function dispatchAgentRunFromGateway(params: {
         timeoutPhase,
         providerStarted: result?.meta?.providerStarted,
       });
+      const responseTimedOut = aborted || terminalOutcome.status === "timeout";
       if (taskTracked) {
         tryFinalizeTrackedAgentTask({
           runId: params.runId,
-          status: aborted ? resolveAbortedAgentTaskStatus(stopReason) : "succeeded",
-          terminalSummary: aborted ? "aborted" : "completed",
+          status: aborted
+            ? resolveAbortedAgentTaskStatus(stopReason)
+            : responseTimedOut
+              ? "timed_out"
+              : "succeeded",
+          terminalSummary: responseTimedOut ? "aborted" : "completed",
           log: params.context.logGateway,
         });
       }
       const payload = {
         runId: params.runId,
-        status: aborted ? ("timeout" as const) : ("ok" as const),
-        summary: aborted ? "aborted" : "completed",
-        ...(aborted ? { stopReason } : {}),
-        ...(aborted && terminalOutcome.timeoutPhase
+        status: responseTimedOut ? ("timeout" as const) : ("ok" as const),
+        summary: responseTimedOut ? "aborted" : "completed",
+        ...(responseTimedOut && terminalOutcome.stopReason
+          ? { stopReason: terminalOutcome.stopReason }
+          : {}),
+        ...(responseTimedOut && terminalOutcome.timeoutPhase
           ? { timeoutPhase: terminalOutcome.timeoutPhase }
           : {}),
-        ...(aborted && terminalOutcome.providerStarted !== undefined
+        ...(responseTimedOut && terminalOutcome.providerStarted !== undefined
           ? { providerStarted: terminalOutcome.providerStarted }
           : {}),
         result,