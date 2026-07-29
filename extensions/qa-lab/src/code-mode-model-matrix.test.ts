8b883b2ab2c4f8be69f9a3cab190abdf9d194aa9
Peter Steinberger <steipete@gmail.com>
2026-07-29 00:56:32 -0700

    test: remove stale code mode import

--- extensions/qa-lab/src/code-mode-model-matrix.test.ts
+++ extensions/qa-lab/src/code-mode-model-matrix.test.ts
@@ -11,7 +11,6 @@ import {
   reserveCodeModeMatrixOutputDir,
   resolveCodeModeMatrixOutputDir,
   runCodeModeModelMatrix,
-  validateQaEvidenceSummaryJson,
   type CodeModeMatrixCellResult,
 } from "../../../scripts/code-mode-model-matrix.ts";
 
