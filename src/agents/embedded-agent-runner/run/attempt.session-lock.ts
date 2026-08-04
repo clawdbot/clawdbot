wang1314-coder/openclaw

fix: honor configured provider retries in embedded prompt window (#87180)
The embedded prompt retry default wrapper injected a hard maxRetries:0 for
SDK calls inside the prompt-lock release window. The sdk.ts seam then computed
`optionsLocal?.maxRetries ?? providerRetrySettings.maxRetries`, so the injected
0 shadowed any configured settings.retry.provider.maxRetries (0 ?? 3 = 0).
Users who set provider retries silently lost them for embedded prompt
submissions.

installEmbeddedPromptRetryDefault now accepts the configured provider retry and
injects it as the default; the install site in attempt.ts passes
settingsManager.getProviderRetrySettings().maxRetries. Precedence is now:
explicit per-call options.maxRetries > configured provider maxRetries > 0.
Unconfigured still defaults to 0 so SDK retries stay out of the lock-release
window where model-fallback owns retries and SDK retries race with session
takeover.

Adds regression coverage for the wrapper defaults/override and a chain test
proving the configured provider maxRetries forwards through to the native SDK
request options.

src/agents/embedded-agent-runner/run/attempt.session-lock.ts
@@ -1230,11 +1230,16 @@ export function installPromptSubmissionLockRelease(params: {
   agent.streamFn = wrappedStreamFn;
 }
 
-// Default to disabling SDK-level retries inside the embedded prompt lock window:
-// model-fallback owns retries, and SDK retries here race with session takeover.
-// This is a default only — an explicit settings.retry.provider.maxRetries (surfaced
-// via options.maxRetries) still wins, so the caller-provided options spread last.
-export function installEmbeddedPromptRetryDefault(session: unknown): void {
+// Inject the provider retry default for SDK calls inside the embedded prompt lock
+// window. The injected default is the configured settings.retry.provider.maxRetries
+// (passed in by the caller); when unset it falls back to 0 so SDK retries stay out of
+// the prompt-lock release window where model-fallback owns retries and SDK retries
+// race with session takeover. Explicit per-call options.maxRetries still wins because
+// the caller-provided options spread last over the injected default.
+export function installEmbeddedPromptRetryDefault(
+  session: unknown,
+  options?: { maxRetries?: number },
+): void {
   const agent = (session as SessionWithAgentPrompt).agent;
   if (typeof agent?.streamFn !== "function") {
     return;
@@ -1243,11 +1248,12 @@ export function installEmbeddedPromptRetryDefault(session: unknown): void {
   if (currentStreamFn["__openclawEmbeddedPromptRetryDefaultInstalled"] === true) {
     return;
   }
+  const defaultMaxRetries = options?.maxRetries ?? 0;
   const innerStreamFn = currentStreamFn;
   const wrappedStreamFn: PromptReleaseStreamFn = (...args: unknown[]) => {
-    const [model, context, options] = args;
-    const requestOptions = options as { maxRetries?: number } | undefined;
-    return innerStreamFn(model, context, { maxRetries: 0, ...requestOptions });
+    const [model, context, callOptions] = args;
+    const requestOptions = callOptions as { maxRetries?: number } | undefined;
+    return innerStreamFn(model, context, { maxRetries: defaultMaxRetries, ...requestOptions });
   };
   wrappedStreamFn["__openclawEmbeddedPromptRetryDefaultInstalled"] = true;
   // The outermost wrapper hides inner markers, so carry the lock-release marker

clawdbot/clawdbot

src/agents/embedded-agent-runner/run/attempt.session-lock.ts
@@ -601,11 +601,16 @@ export function installPromptSubmissionLockRelease(params: {
   agent.streamFn = wrappedStreamFn;
 }
 
-// Default to disabling SDK-level retries inside the embedded prompt lock window:
-// model-fallback owns retries, and SDK retries here race with session takeover.
-// This is a default only — an explicit settings.retry.provider.maxRetries (surfaced
-// via options.maxRetries) still wins, so the caller-provided options spread last.
-export function installEmbeddedPromptRetryDefault(session: unknown): void {
+// Inject the provider retry default for SDK calls inside the embedded prompt lock
+// window. The injected default is the configured settings.retry.provider.maxRetries
+// (passed in by the caller); when unset it falls back to 0 so SDK retries stay out of
+// the prompt-lock release window where model-fallback owns retries and SDK retries
+// race with session takeover. Explicit per-call options.maxRetries still wins because
+// the caller-provided options spread last over the injected default.
+export function installEmbeddedPromptRetryDefault(
+  session: unknown,
+  defaults?: { maxRetries?: number },
+): void {
   const agent = (session as SessionWithAgentPrompt).agent;
   if (typeof agent?.streamFn !== "function") {
     return;
@@ -614,6 +619,7 @@ export function installEmbeddedPromptRetryDefault(session: unknown): void {
   if (currentStreamFn.openclawEmbeddedPromptRetryDefaultInstalled === true) {
     return;
   }
+  const defaultMaxRetries = defaults?.maxRetries ?? 0;
   const innerStreamFn = currentStreamFn;
   const wrappedStreamFn: PromptReleaseStreamFn = (...args: unknown[]) => {
     const [model, context, options] = args as [
@@ -621,7 +627,7 @@ export function installEmbeddedPromptRetryDefault(session: unknown): void {
       unknown,
       { maxRetries?: number } | undefined,
     ];
-    return innerStreamFn(model, context, { maxRetries: 0, ...options });
+    return innerStreamFn(model, context, { maxRetries: defaultMaxRetries, ...options });
   };
   wrappedStreamFn.openclawEmbeddedPromptRetryDefaultInstalled = true;
   // The outermost wrapper hides inner markers, so carry the lock-release marker