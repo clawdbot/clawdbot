Jerry-Xin/openclaw

fix: prevent silent message loss from EmbeddedAttemptSessionTakeoverError (#87180)
When the OpenAI SDK retried an ECONNRESET internally (default maxRetries=2),
the session write lock was released during the retry window. Incoming steering
messages could then modify the transcript without updating the fence, causing
a fingerprint mismatch on reacquire. The resulting
EmbeddedAttemptSessionTakeoverError was caught by model-fallback which aborted
the run, and the outer reply pipeline produced no user-visible output — the
message was silently lost.

Four coordinated fixes:

1. Wrap steering writes in withOwnedSessionTranscriptWrites so fence tracking
   stays consistent even when steering arrives during prompt lock release.

2. Add a dedicated catch for EmbeddedAttemptSessionTakeoverError in
   runAgentTurnWithFallback that returns a user-friendly resend prompt
   instead of falling into the generic failure branch.

3. Disable OpenAI SDK internal retries (maxRetries: 0) across all three
   transport paths (openai-completions, openai-responses-shared, and
   openai-transport-stream) so OpenClaw controls retry logic at the
   higher model-fallback level.

4. Add phase tracking to EmbeddedAttemptSessionTakeoverError for
   better diagnostics (prompt_reacquire, cleanup, write, fence_check).

Fixes #87180

src/llm/providers/openai-completions.test.ts
@@ -14,13 +14,15 @@ type OpenAICompatibleChatCompletionChunk = Omit<DeepPartial<ChatCompletionChunk>
 };
 
 const mockChunksRef: { chunks: OpenAICompatibleChatCompletionChunk[] } = { chunks: [] };
+const mockRequestOptionsRef: { options: unknown[] } = { options: [] };
 
 vi.mock("openai", () => {
   class MockOpenAI {
     chat = {
       completions: {
-        create: () => ({
+        create: (_params: unknown, options: unknown) => ({
           withResponse: async () => {
+            mockRequestOptionsRef.options.push(options);
             async function* generate() {
               for (const chunk of mockChunksRef.chunks) {
                 yield chunk;
@@ -111,6 +113,20 @@ function makeFinishChunk(
 }
 
 describe("OpenAI-compatible completions params", () => {
+  it("disables OpenAI SDK retries for streaming requests by default", async () => {
+    mockChunksRef.chunks = [makeTextChunk("Hello"), makeFinishChunk("stop")];
+    mockRequestOptionsRef.options = [];
+
+    const stream = streamOpenAICompletions(model, context, {
+      apiKey: "sk-test",
+    });
+    const result = await stream.result();
+
+    expect(result.stopReason).toBe("stop");
+    expect(mockRequestOptionsRef.options).toHaveLength(1);
+    expect(mockRequestOptionsRef.options[0]).toMatchObject({ maxRetries: 0 });
+  });
+
   it("clamps requested max tokens to the model output cap", async () => {
     let capturedMaxTokens: unknown;
     const stream = streamOpenAICompletions(createModel(32_000), context, {

clawdbot/clawdbot

packages/ai/src/providers/openai-completions.test.ts
@@ -43,6 +43,7 @@ const mockOpenAIOptionsRef: { options: unknown[]; payloads: unknown[]; requests:
   payloads: [],
   requests: [],
 };
+const mockRequestOptionsRef: { options: unknown[] } = { options: [] };
 
 vi.mock("openai", () => {
   class MockOpenAI {
@@ -55,6 +56,7 @@ vi.mock("openai", () => {
         create: (params: unknown, requestOptions: unknown) => {
           mockOpenAIOptionsRef.payloads.push(params);
           mockOpenAIOptionsRef.requests.push(requestOptions);
+          mockRequestOptionsRef.options.push(requestOptions);
           return {
             withResponse: async () => {
               if (mockChunksRef.stream) {
@@ -813,6 +815,20 @@ describe("OpenAI-compatible completions params", () => {
 
     expect(result.stopReason).toBe("error");
     expect(capturedPayload).not.toHaveProperty("tools");
+  });
+
+  it("disables OpenAI SDK retries for streaming requests by default", async () => {
+    mockChunksRef.chunks = [makeTextChunk("Hello"), makeFinishChunk("stop")];
+    mockRequestOptionsRef.options = [];
+
+    const stream = streamOpenAICompletions(model, context, {
+      apiKey: "sk-test",
+    });
+    const result = await stream.result();
+
+    expect(result.stopReason).toBe("stop");
+    expect(mockRequestOptionsRef.options).toHaveLength(1);
+    expect(mockRequestOptionsRef.options[0]).toMatchObject({ maxRetries: 0 });
   });
 
   it("clamps requested max tokens to the model output cap", async () => {