a2752ad12c67924a50b51ccd87416c1d6397318f
Vincent Koc <vincentkoc@ieee.org>
2026-08-07 06:25:09 +0200

    fix(plugins): keep loader authority type private

--- src/plugins/loader-module-runtime.ts
+++ src/plugins/loader-module-runtime.ts
@@ -20,7 +20,7 @@ import {
 } from "./sdk-alias.js";
 import type { OpenClawPluginApi, OpenClawPluginDefinition } from "./types.js";
 
-export type PluginModuleLoadAuthority = {
+type PluginModuleLoadAuthority = {
   trustedInstalledPrivateSdkOwner?: string;
 };
 
