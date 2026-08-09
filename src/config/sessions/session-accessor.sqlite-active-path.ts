ZYV5ge/openclaw

fix: avoid false branch-switch errors after background updates

src/config/sessions/session-accessor.sqlite-active-path.ts
@@ -1,0 +1,21 @@
+import { withCurrentProjectionSnapshot } from "./session-accessor.sqlite-active-events.js";
+import type { SessionTranscriptReadScope } from "./session-accessor.sqlite-contract.js";
+import { isSessionTranscriptEventOnActivePath } from "./session-transcript-index.js";
+
+/** Checks one entry against the current active path without materializing that path. */
+export function readSessionTranscriptActivePathEntryState(
+  scope: SessionTranscriptReadScope,
+  entryId: string,
+): {
+  activeLeafEntryId: string | null;
+  entryOnActivePath: boolean;
+} {
+  return withCurrentProjectionSnapshot(scope, ({ database, resolved, state }) => ({
+    activeLeafEntryId: state.leafEventId,
+    entryOnActivePath: isSessionTranscriptEventOnActivePath(
+      database.db,
+      resolved.sessionId,
+      entryId,
+    ),
+  }));
+}

clawdbot/clawdbot

src/config/sessions/session-accessor.sqlite-active-path.ts
@@ -1,0 +1,21 @@
+import { withCurrentProjectionSnapshot } from "./session-accessor.sqlite-active-projection.js";
+import type { SessionTranscriptReadScope } from "./session-accessor.sqlite-contract.js";
+import { isSessionTranscriptEventOnActivePath } from "./session-transcript-index.js";
+
+/** Checks one entry against the current active path without materializing that path. */
+export function readSessionTranscriptActivePathEntryState(
+  scope: SessionTranscriptReadScope,
+  entryId: string,
+): {
+  activeLeafEntryId: string | null;
+  entryOnActivePath: boolean;
+} {
+  return withCurrentProjectionSnapshot(scope, ({ database, resolved, state }) => ({
+    activeLeafEntryId: state.leafEventId,
+    entryOnActivePath: isSessionTranscriptEventOnActivePath(
+      database.db,
+      resolved.sessionId,
+      entryId,
+    ),
+  }));
+}