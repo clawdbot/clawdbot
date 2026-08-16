openclaw/openclaw

refactor(googlechat,zalouser): declare single-account config promotion keys
Declare singleAccountKeysToMove on both channel setup adapters and add
configPromotion setupFeature to their package manifests, completing the
per-channel migration started in #112293.

Co-Authored-By: Claude <noreply@anthropic.com>

extensions/googlechat/src/setup-core.ts
@@ -6,36 +6,46 @@ import {
 
 const channel = "googlechat" as const;
 
-export const googlechatSetupAdapter = createPatchedAccountSetupAdapter({
-  channelKey: channel,
-  validateInput: createSetupInputPresenceValidator({
-    defaultAccountOnlyEnvError:
-      "GOOGLE_CHAT_SERVICE_ACCOUNT env vars can only be used for the default account.",
-    whenNotUseEnv: [
-      {
-        someOf: ["token", "tokenFile"],
-        message: "Google Chat requires --token (service account JSON) or --token-file.",
-      },
-    ],
+export const googlechatSetupAdapter = {
+  ...createPatchedAccountSetupAdapter({
+    channelKey: channel,
+    validateInput: createSetupInputPresenceValidator({
+      defaultAccountOnlyEnvError:
+        "GOOGLE_CHAT_SERVICE_ACCOUNT env vars can only be used for the default account.",
+      whenNotUseEnv: [
+        {
+          someOf: ["token", "tokenFile"],
+          message: "Google Chat requires --token (service account JSON) or --token-file.",
+        },
+      ],
+    }),
+    buildPatch: (input) => {
+      const patch = input.useEnv
+        ? {}
+        : input.tokenFile
+          ? { serviceAccountFile: input.tokenFile }
+          : input.token
+            ? { serviceAccount: input.token }
+            : {};
+      const audienceType = input.audienceType?.trim();
+      const audience = input.audience?.trim();
+      const webhookPath = input.webhookPath?.trim();
+      const webhookUrl = input.webhookUrl?.trim();
+      return {
+        ...patch,
+        ...(audienceType ? { audienceType } : {}),
+        ...(audience ? { audience } : {}),
+        ...(webhookPath ? { webhookPath } : {}),
+        ...(webhookUrl ? { webhookUrl } : {}),
+      };
+    },
   }),
-  buildPatch: (input) => {
-    const patch = input.useEnv
-      ? {}
-      : input.tokenFile
-        ? { serviceAccountFile: input.tokenFile }
-        : input.token
-          ? { serviceAccount: input.token }
-          : {};
-    const audienceType = input.audienceType?.trim();
-    const audience = input.audience?.trim();
-    const webhookPath = input.webhookPath?.trim();
-    const webhookUrl = input.webhookUrl?.trim();
-    return {
-      ...patch,
-      ...(audienceType ? { audienceType } : {}),
-      ...(audience ? { audience } : {}),
-      ...(webhookPath ? { webhookPath } : {}),
-      ...(webhookUrl ? { webhookUrl } : {}),
-    };
-  },
-});
+  singleAccountKeysToMove: [
+    "serviceAccount",
+    "serviceAccountFile",
+    "audienceType",
+    "audience",
+    "webhookPath",
+    "webhookUrl",
+  ],
+};

extensions/zalouser/src/channel.setup.test.ts
@@ -11,6 +11,10 @@ import { zalouserSetupPlugin } from "./setup-test-helpers.js";
 const zalouserSetupGetStatus = createPluginSetupWizardStatus(zalouserSetupPlugin);
 
 describe("zalouser setup plugin", () => {
+  it("exposes config-promotion declarations on the setup adapter", () => {
+    expect(zalouserSetupPlugin.setup.singleAccountKeysToMove).toEqual([]);
+  });
+
   it("builds setup status without an initialized runtime", async () => {
     const stateDir = await mkdtemp(path.join(os.tmpdir(), "openclaw-zalouser-setup-"));
 

clawdbot/clawdbot

extensions/googlechat/src/setup-core.ts
@@ -15,40 +15,50 @@ type GoogleChatSetupInput = ChannelSetupInput & {
   webhookUrl?: string;
 };
 
-export const googlechatSetupAdapter = createPatchedAccountSetupAdapter({
-  channelKey: channel,
-  validateInput: createSetupInputPresenceValidator({
-    defaultAccountOnlyEnvError:
-      "GOOGLE_CHAT_SERVICE_ACCOUNT env vars can only be used for the default account.",
-    whenNotUseEnv: [
-      {
-        someOf: ["token", "tokenFile"],
-        message: "Google Chat requires --token (service account JSON) or --token-file.",
-      },
-    ],
+export const googlechatSetupAdapter = {
+  ...createPatchedAccountSetupAdapter({
+    channelKey: channel,
+    validateInput: createSetupInputPresenceValidator({
+      defaultAccountOnlyEnvError:
+        "GOOGLE_CHAT_SERVICE_ACCOUNT env vars can only be used for the default account.",
+      whenNotUseEnv: [
+        {
+          someOf: ["token", "tokenFile"],
+          message: "Google Chat requires --token (service account JSON) or --token-file.",
+        },
+      ],
+    }),
+    buildPatch: (input) => {
+      const setupInput = input as GoogleChatSetupInput;
+      const patch = setupInput.useEnv
+        ? {}
+        : setupInput.tokenFile
+          ? { serviceAccountFile: setupInput.tokenFile }
+          : setupInput.token
+            ? { serviceAccount: setupInput.token }
+            : {};
+      const audienceType = setupInput.audienceType?.trim();
+      const audience = setupInput.audience?.trim();
+      const webhookPath = setupInput.webhookPath?.trim();
+      const webhookUrl = setupInput.webhookUrl?.trim();
+      return {
+        ...patch,
+        ...(audienceType ? { audienceType } : {}),
+        ...(audience ? { audience } : {}),
+        ...(webhookPath ? { webhookPath } : {}),
+        ...(webhookUrl ? { webhookUrl } : {}),
+      };
+    },
   }),
-  buildPatch: (input) => {
-    const setupInput = input as GoogleChatSetupInput;
-    const patch = setupInput.useEnv
-      ? {}
-      : setupInput.tokenFile
-        ? { serviceAccountFile: setupInput.tokenFile }
-        : setupInput.token
-          ? { serviceAccount: setupInput.token }
-          : {};
-    const audienceType = setupInput.audienceType?.trim();
-    const audience = setupInput.audience?.trim();
-    const webhookPath = setupInput.webhookPath?.trim();
-    const webhookUrl = setupInput.webhookUrl?.trim();
-    return {
-      ...patch,
-      ...(audienceType ? { audienceType } : {}),
-      ...(audience ? { audience } : {}),
-      ...(webhookPath ? { webhookPath } : {}),
-      ...(webhookUrl ? { webhookUrl } : {}),
-    };
-  },
-});
+  singleAccountKeysToMove: [
+    "serviceAccount",
+    "serviceAccountFile",
+    "audienceType",
+    "audience",
+    "webhookPath",
+    "webhookUrl",
+  ],
+};
 
 export const googlechatSetupContract = defineChannelSetupContract({
   fields: {

extensions/zalouser/src/channel.setup.test.ts
@@ -11,6 +11,10 @@ import { zalouserSetupPlugin } from "./setup-test-helpers.js";
 const zalouserSetupGetStatus = createPluginSetupWizardStatus(zalouserSetupPlugin);
 
 describe("zalouser setup plugin", () => {
+  it("exposes config-promotion declarations on the setup adapter", () => {
+    expect(zalouserSetupPlugin.setupContract.singleAccountKeysToMove).toEqual([]);
+  });
+
   it("builds setup status without an initialized runtime", async () => {
     const stateDir = await mkdtemp(path.join(os.tmpdir(), "openclaw-zalouser-setup-"));
 