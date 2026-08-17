openclaw/openclaw

fix(mcp): skip provider discovery in isolated runtime

src/agents/agent-auth-discovery.external-cli.test.ts
@@ -113,4 +113,15 @@ describe("resolveAgentCredentialsForDiscovery external CLI scoping", () => {
       },
     });
   });
+
+  it("does not load synthetic provider auth when providers are disabled", () => {
+    syntheticAuthMocks.resolveRuntimeSyntheticAuthProviderRefs.mockReturnValue(["fireworks"]);
+
+    resolveAgentCredentialsForDiscovery("/tmp/openclaw-agent", {
+      env: { OPENCLAW_SKIP_PROVIDERS: "1" },
+    });
+
+    expect(syntheticAuthMocks.resolveRuntimeSyntheticAuthProviderRefs).not.toHaveBeenCalled();
+    expect(syntheticAuthMocks.resolveProviderSyntheticAuthWithPlugin).not.toHaveBeenCalled();
+  });
 });

clawdbot/clawdbot

src/agents/agent-auth-discovery.external-cli.test.ts
@@ -161,4 +161,15 @@ describe("resolveAgentDiscoveryAuthFacts external CLI scoping", () => {
       expect(syntheticAuthMocks.resolveProviderSyntheticAuthWithPlugin).not.toHaveBeenCalled();
     },
   );
+
+  it("does not load synthetic provider auth when providers are disabled", () => {
+    syntheticAuthMocks.resolveRuntimeSyntheticAuthProviderRefs.mockReturnValue(["fireworks"]);
+
+    resolveAgentDiscoveryAuthFacts("/tmp/openclaw-agent", {
+      env: { OPENCLAW_SKIP_PROVIDERS: "1" },
+    });
+
+    expect(syntheticAuthMocks.resolveRuntimeSyntheticAuthProviderRefs).not.toHaveBeenCalled();
+    expect(syntheticAuthMocks.resolveProviderSyntheticAuthWithPlugin).not.toHaveBeenCalled();
+  });
 });