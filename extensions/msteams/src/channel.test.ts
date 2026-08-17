154a9b0239778d41af62b519b414688c983ff784
Jimmy Puckett <jimmy.puckett@spinen.com>
2026-08-17 13:08:37 -0400

    fix(msteams): restore stable host contract

--- extensions/msteams/src/channel.test.ts
+++ extensions/msteams/src/channel.test.ts
@@ -9,7 +9,6 @@ import {
   resolveMSTeamsRuntimeAccount,
 } from "./accounts.js";
 import { msTeamsApprovalAuth } from "./approval-auth.js";
-import { createMSTeamsSecurityWarningCollector } from "./channel-security.js";
 import { msteamsPlugin } from "./channel.js";
 import { msteamsSetupPlugin } from "./channel.setup.js";
 
@@ -225,16 +224,6 @@ describe("msteamsPlugin", () => {
         account: msteamsPlugin.config.resolveAccount(cfg, "default"),
       }),
     ).toEqual([]);
-
-    const legacyWarnings = createMSTeamsSecurityWarningCollector(undefined)({
-      cfg,
-      accountId: "support",
-    });
-    expect(legacyWarnings).toEqual([
-      expect.stringMatching(
-        /MS Teams\[support\].*channels\.msteams\.accounts\.support\.groupPolicy.*channels\.msteams\.accounts\.support\.groupAllowFrom/,
-      ),
-    ]);
   });
 
   it("does not advertise message tools for disabled or unconfigured named accounts", () => {
