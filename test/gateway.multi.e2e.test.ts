0a165c26e0c4d7cf82c3bb45612fff0c36a5160c
qingminlong <qing.minlong@xydigit.com>
2026-09-03 20:13:15 +0800

    test(gateway): use public gateway client helper

--- test/gateway.multi.e2e.test.ts
+++ test/gateway.multi.e2e.test.ts
@@ -7,6 +7,7 @@ import { WebSocket } from "ws";
 import { afterAll, describe, expect, it } from "vitest";
 import { GatewayClient } from "../src/gateway/client.js";
 import { buildDeviceAuthPayloadV3 } from "../src/gateway/device-auth.js";
+import { connectGatewayClient } from "../src/gateway/test-helpers.e2e.js";
 import {
   loadOrCreateDeviceIdentity,
   publicKeyRawBase64UrlFromPem,
