openclaw/openclaw

fix(ui): preserve Windows catalog root kinds

ui/src/lib/sessions/catalog-project-grouping.ts
@@ -13,15 +13,31 @@ type CatalogProjectGroup = {
   sessions: SessionCatalogSession[];
 };
 
+type WindowsPathRootKind = "drive" | "unc" | "rooted";
+
+function windowsPathRootKind(value: string): WindowsPathRootKind | undefined {
+  if (/^[A-Za-z]:[\\/]/.test(value)) {
+    return "drive";
+  }
+  if (value.startsWith("\\\\")) {
+    return "unc";
+  }
+  if (value.startsWith("\\")) {
+    return "rooted";
+  }
+  return undefined;
+}
+
 function isWindowsPath(value: string): boolean {
-  return /^[A-Za-z]:[\\/]/.test(value) || value.startsWith("\\");
+  return windowsPathRootKind(value) !== undefined;
 }
 
 function catalogProjectPathIdentity(value: string): string {
-  if (!isWindowsPath(value)) {
+  const rootKind = windowsPathRootKind(value);
+  if (!rootKind) {
     return value;
   }
-  return `windows:${value
+  return `windows:${rootKind}:${value
     .split(/[\\/]+/)
     .filter(Boolean)
     .map((segment) => segment.toLowerCase())
@@ -58,10 +74,11 @@ export function groupCatalogSessionsByProject(sessions: readonly SessionCatalogS
       group.sessions.push(session);
       continue;
     }
-    // Accepted tradeoff: filesystem-root cwds ("/", "C:\") are not real harness
-    // session roots; after trimming they fall to the ungrouped flat tail by design.
+    // Accepted tradeoff: local filesystem-root cwds ("/", "C:\") are not real
+    // harness session roots, so they fall to the ungrouped flat tail. UNC share
+    // roots remain groupable because the share itself can be a project root.
     let projectPath = session.cwd?.trim().replace(/[\\/]+$/, "");
-    if (!projectPath) {
+    if (!projectPath || /^[A-Za-z]:$/.test(projectPath)) {
       ungrouped.push(session);
       continue;
     }
@@ -72,7 +89,7 @@ export function groupCatalogSessionsByProject(sessions: readonly SessionCatalogS
       : /^(.*?)[\\/]\.claude[\\/]worktrees[\\/][^\\/]/;
     const worktreeMatch = projectPath.match(worktreePattern);
     projectPath = worktreeMatch?.[1] ?? projectPath;
-    if (!projectPath) {
+    if (!projectPath || /^[A-Za-z]:$/.test(projectPath)) {
       ungrouped.push(session);
       continue;
     }

clawdbot/clawdbot

ui/src/lib/sessions/catalog-project-grouping.ts
@@ -28,14 +28,15 @@ export function sessionActorGroupId(owner: SessionCreatedActor | undefined): str
 // paths or worktrees with no origin repo.
 export function foldWorktreeCheckoutPath(path: string): string | null {
   const trimmed = path.replace(/[\\/]+$/, "");
-  if (!trimmed) {
+  if (!trimmed || /^[A-Za-z]:$/.test(trimmed)) {
     return null;
   }
   const pattern = isWindowsPath(trimmed)
     ? /^(.*?)[\\/]\.claude[\\/]worktrees[\\/][^\\/]/i
     : /^(.*?)[\\/]\.claude[\\/]worktrees[\\/][^\\/]/;
   const match = trimmed.match(pattern);
-  return match ? match[1] || null : trimmed;
+  const folded = match ? match[1] || null : trimmed;
+  return folded && !/^[A-Za-z]:$/.test(folded) ? folded : null;
 }
 
 /** Basename shown for a checkout path in project sections. */
@@ -54,15 +55,31 @@ type CatalogProjectGroup = {
   sessions: SessionCatalogSession[];
 };
 
+type WindowsPathRootKind = "drive" | "unc" | "rooted";
+
+function windowsPathRootKind(value: string): WindowsPathRootKind | undefined {
+  if (/^[A-Za-z]:[\\/]/.test(value)) {
+    return "drive";
+  }
+  if (value.startsWith("\\\\")) {
+    return "unc";
+  }
+  if (value.startsWith("\\")) {
+    return "rooted";
+  }
+  return undefined;
+}
+
 function isWindowsPath(value: string): boolean {
-  return /^[A-Za-z]:[\\/]/.test(value) || value.startsWith("\\");
+  return windowsPathRootKind(value) !== undefined;
 }
 
 function catalogProjectPathIdentity(value: string): string {
-  if (!isWindowsPath(value)) {
+  const rootKind = windowsPathRootKind(value);
+  if (!rootKind) {
     return value;
   }
-  return `windows:${value
+  return `windows:${rootKind}:${value
     .split(/[\\/]+/)
     .filter(Boolean)
     .map((segment) => segment.toLowerCase())