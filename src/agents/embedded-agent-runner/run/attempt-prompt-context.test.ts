zhangguiping-xydt/openclaw

fix(agents): keep runtime-only context out of user turns

src/agents/embedded-agent-runner/run/attempt-prompt-context.test.ts
@@ -240,7 +240,7 @@ describe("prepareEmbeddedAttemptPromptContext", () => {
     expect(hoisted.warn).not.toHaveBeenCalled();
   });
 
-  it("moves runtime-only context into the active system prompt", () => {
+  it("keeps runtime-only inbound context out of the visible prompt", () => {
     const fixture = createInput({
       attempt: createAttempt({
         currentInboundContext: { text: "Room event metadata" },
@@ -258,8 +258,11 @@ describe("prepareEmbeddedAttemptPromptContext", () => {
     const result = prepareEmbeddedAttemptPromptContext(fixture.input);
 
     expect(result.promptSubmission.runtimeOnly).toBe(true);
-    expect(result.promptForSession).toContain("Room event metadata");
-    expect(result.runtimeContextMessageForCurrentTurn).toBeUndefined();
+    expect(result.promptForSession).toBe("Continue the OpenClaw runtime event.");
+    expect(result.promptForModel).toBe("Continue the OpenClaw runtime event.");
+    expect(result.promptForSession).not.toContain("Room event metadata");
+    expect(result.promptForModel).not.toContain("Room event metadata");
+    expect(result.runtimeContextMessageForCurrentTurn?.content).toContain("Room event metadata");
     expect(result.systemPromptForHook).toContain("Runtime room event");
     expect(fixture.setActiveSessionSystemPrompt).toHaveBeenCalledWith(
       expect.stringContaining("Runtime room event"),

clawdbot/clawdbot

src/agents/embedded-agent-runner/run/attempt-prompt-context.test.ts
@@ -443,7 +443,7 @@ describe("prepareEmbeddedAttemptPromptContext", () => {
   });
 
   it.each([3, 4])(
-    "keeps version %s runtime-only events in system context with current facts",
+    "keeps version %s runtime-only events in system context with inbound context in the hidden carrier",
     (sessionVersion) => {
       const fixture = createInput({
         attempt: createAttempt({
@@ -464,11 +464,14 @@ describe("prepareEmbeddedAttemptPromptContext", () => {
 
       expect(result.systemPromptForHook).toContain("OpenClaw runtime event.");
       expect(result.promptSubmission.runtimeOnly).toBe(true);
-      expect(result.promptForSession).toBe(
-        "Room conversation data\n\nContinue the OpenClaw runtime event.",
-      );
+      expect(result.promptForSession).toBe("Continue the OpenClaw runtime event.");
       expect(result.promptForModel).toBe(result.promptForSession);
+      expect(result.promptForSession).not.toContain("Room conversation data");
+      expect(result.promptForModel).not.toContain("Room conversation data");
       expect(result.systemPromptForHook).not.toContain("Room conversation data");
+      expect(result.runtimeContextMessageForCurrentTurn?.content).toContain(
+        "Room conversation data",
+      );
       expect(result.runtimeContextMessageForCurrentTurn?.content).toContain(
         "Active exec sessions:\nnone",
       );